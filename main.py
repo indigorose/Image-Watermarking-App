ea470fc55fea764769fd7705dbcce97c32b2f52f
indigorose <indigorose049@gmail.com>
2022-04-24 11:24:49 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+# ------------- Python Code -------------- #
+
+
+# --------------- UI Setup --------------- #
+# Constants
+BACKGROUND_COLOR = '#F7F5F2'
+COLOR_1 = '#8D8DAA'
+COLOR_2 = '#DFDFDE'
+COLOR_3 = '#F56D91'
+
+window = Tk()
+window.title('Image Watermarking App')
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+window.mainloop()
